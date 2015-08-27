7c7d250c8f3994bbf651b8cad07fba17109348a2
Claus <claus@umbraco.com>
2015-08-27 15:06:02 +0200

    Fixes: COU-197 Form is not picked up when included in a document by form picker
    Resolver was not declared public - it wasn't picked up by Courier.

--- Umbraco.Courier.FormsProvider/FormPickerResolver.cs
+++ Umbraco.Courier.FormsProvider/FormPickerResolver.cs
@@ -1,19 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Umbraco.Courier.Core;
+using Umbraco.Courier.DataResolvers;
+using Umbraco.Courier.ItemProviders;
 
 namespace Umbraco.Courier.FormsProvider
 {
-    class FormPickerResolver : Umbraco.Courier.DataResolvers.PropertyDataResolverProvider
+    public class FormPickerResolver : PropertyDataResolverProvider
     {
         public override string EditorAlias
         {
             get { return Constants.FormPickerAlias; }
         }
 
-        public override void PackagingProperty(Core.Item item, ItemProviders.ContentProperty propertyData)
+        public override void PackagingProperty(Item item, ContentProperty propertyData)
         {
             Guid g;
             if (propertyData.Value != null && Guid.TryParse(propertyData.Value.ToString(), out g))
